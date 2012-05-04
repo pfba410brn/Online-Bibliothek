7a16877ac5d2516eb72f04c9e6f738672f95581d
Philipp Renerig <pfba410brn@pb.bib.de>
2012-05-04 11:19:44 +0200

    Hinzugefügt: Java-Doc Beispiel

---
+++ src/javadoc_beispiel.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Beschreibung der Klasse javadoc_beispiel
+ * Was tut die Klasse?
+ * 
+ * @version 0.01
+ * @author philipp.renerig
+ *
+ */
+public class javadoc_beispiel {
+
+	/**
+	 * Beschreibung der Methode
+	 * Was tut diese Methode?
+	 * 
+	 * @param args Was ist das für ein Parameter?
+	 * @retrun void
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Java Doc!");
+	}
+}
