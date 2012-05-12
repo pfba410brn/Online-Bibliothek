fb86cfda86b1fd9404462f3b471d6b0b6b385983
Philipp Renerig <pfba410brn@pb.bib.de>
2012-05-12 02:09:19 +0200

    y<x

---
+++ src/controller/LoginController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginController extends HttpServlet {
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		super.doPost(req, resp);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/controller/MainController.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class MainController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
