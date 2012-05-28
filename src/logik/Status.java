dc832310346007be9290917f7c96332240da79af
Sina <Sina@Sina-PC>
2012-05-28 16:49:18 +0200

    Geändert

--- src/logik/Status.java
+++ src/logik/Status.java
@@ -2,12 +2,19 @@ package logik;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import db.Benutzer;
+import db.Buch;
+import db.DbVerwaltung;
+import db.Exemplar;
 
 
 
@@ -19,24 +26,82 @@ public class Status extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-
+	private Benutzer benutzer;
+	private List<Exemplar> exemlarListe;
    
 	
 	public void doPost(HttpServletRequest request,
             HttpServletResponse response)
             			throws ServletException, IOException {
-		
+		response.setContentType("text/html");
+		DbVerwaltung db = new DbVerwaltung();
+		PrintWriter out = response.getWriter();
 		if(request.getParameter("isbn") != null) {
 			
 			/* NEUES BUCH HINZUFÜGEN */
-			PrintWriter out = response.getWriter();
-			out.println(request.getParameter("isbn"));
+			String isbn = request.getParameter("isbn");
+			//out.println(isbn);
+			
+			Buch buch = db.select_BuchUeberISBN(isbn);
+			List<Exemplar> exemplarListe =  db.selectAll_Exemplar();
+			for (Exemplar exemplar : exemplarListe)
+			{
+				if (exemplar.getBuch().equals(buch))
+				{
+					this.exemlarListe.add(exemplar);
+					break;
+				}
+			}
 		}
 		
 		if(request.getParameter("kundennr") != null) {
-			/* NEUEN KUNDEN HINZUFÜGEN */
+			/* NEUEN KUNDEN HINZUFÜGEN ????*/
+			String kundennr = request.getParameter("kundennr");
+			this.benutzer = db.select_BenutzerUeberID(kundennr);
+			
+			if(request.getParameter("do").equals("mediumHinzufuegen")) {
+				//out.print("<h1>Hey das geht </h1>" + buch.getIsbn() + " mit dieser ISBN!!!");
+				for (Exemplar exemplar : this.exemlarListe){
+				
+					out.print("<div style=\"width:190px; float:right;\">");
+					out.print("<table width=\"190px\">");
+					out.print("<tr><td><b>" + exemplar.getBuch().getTitel() + "</b></td></tr>");
+					out.print("<tr><td>" + exemplar.getBuch().getAutor() + "</td></tr>");
+					out.print("<tr><td>" +exemplar.getBuch().getIsbn() + "</td></tr>");
+					out.print("</table>");
+					out.print("</div>");
+					out.print("<div style=\"width:45px; margin-top:20px;\"><input type=\"image\" name=\"absenden\" src=\"../img/icons/pfeil.png\"></div>");
+					out.print("<div style=\"clear:both;\"></div>");
+					out.print("<hr />");
+				}
+				
+			}
+		}
+		else
+		{
 			
 		}
 	}
+
+
+	private List<Exemplar> getExemlarListe() {
+		return exemlarListe;
+	}
+
+
+	private void setExemlarListe(List<Exemplar> exemlarListe) {
+		this.exemlarListe = exemlarListe;
+	}
+
+
+	private Benutzer getBenutzer() {
+		return benutzer;
+	}
+
+
+	private void setBenutzer(Benutzer benutzer) {
+		this.benutzer = benutzer;
+	}
+	
 	
 }
