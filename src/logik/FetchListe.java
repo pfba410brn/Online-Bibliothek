1235910ff37bec4b6f391f860c879f8c493f39ad
Philipp Renerig <pfba410brn@pb.bib.de>
2012-06-07 14:17:36 +0200

    Commit JavaDoc Änderung

--- src/logik/FetchListe.java
+++ src/logik/FetchListe.java
@@ -14,7 +14,15 @@ import db.DbVerwaltung;
 import db.ExemplarBenutzer;
 
 import logik.JsonConverter;
-
+/**
+ * 
+ * FetchListe holt den einen Bestand aus der Datenbank und gibt
+ * ihn als JSON convertiert an die init.js zurück.
+ * 
+ * @version 1.2
+ * @author philipp.renerig
+ *
+ */
 @WebServlet("/bib/fetchListe")
 public class FetchListe extends HttpServlet {
 
